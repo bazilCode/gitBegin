86eaed65552ff677a4a3035b57ee144cec104716
bazilM <blackknightbazil@gmail.com>
2022-06-15 01:49:48 +0530

    includedIndex_done

--- masterChild.jsx
+++ masterChild.jsx
@@ -1,2 +1,7 @@
-//////masterChild/////
-        // mergerTask_1 merged with masterChild content ====== git merge mergerTask1 masterChild 
+function masterChild(){
+        return(
+            <div>
+                MASTERCHILD
+            </div>
+        )
+    }
